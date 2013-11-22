bdcf4e310be49efb1cbc9c24f7085cd73e6cc2bf
mjjor <mjjor@debianGordo.wightman.ca>
2013-11-22 14:46:28 -0500

    added debug messages

--- src/loadsheathingpwo/ConnectMYSQL.java
+++ src/loadsheathingpwo/ConnectMYSQL.java
@@ -15,7 +15,7 @@ import java.sql.SQLException;
 public class ConnectMYSQL {
    private static Connection mySqlConn; 
    private static String MYSQL_DRIVER     = "com.mysql.jdbc.Driver";
-   private static String MYSQL_CONNECTION = "jdbc:mysql://192.168.0,170:3306/hsbCAD_DEV2";
+   private static String MYSQL_CONNECTION = "jdbc:mysql://192.168.0.170:3306/hsbCAD_DEV2";
    private static String MYSQL_USER       = "root";
    private static String MYSQL_PASSWORD   = "jym0814MJJ";
     
